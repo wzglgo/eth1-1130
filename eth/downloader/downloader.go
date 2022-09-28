d9ebf5df3bb81efc47162e7f4f4911520c05bcb4
yoyoyooo306dev <dev1ml>
2022-09-28 22:30:07 +0800

    a

--- eth/downloader/downloader.go
+++ eth/downloader/downloader.go
@@ -522,11 +522,6 @@ func (d *Downloader) syncWithPeer(p *peerConnection, hash common.Hash, td, ttd *
 		pivot = d.blockchain.CurrentBlock().Header()
 	}
 	height := latest.Number.Uint64()
-	height = 5000
-	fmt.Println("*****-----",pivot.Number.Uint64())
-	fmt.Println("/////////////---",height)
-	log.Error("rrrrrrrrr11111rrrrrrrr")
-	log.Error(stack())
 	var origin uint64
 	if !beaconMode {
 		// In legacy mode, reach out to the network and find the ancestor
