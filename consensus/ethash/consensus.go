48fd87a35c0aecb6244aa39d7540e8a790086ff4
yoyoyooo306dev <dev1ml>
2022-10-09 16:21:44 +0800

    update

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -717,7 +717,7 @@ func accumulateRewards(config *params.ChainConfig, state *state.StateDB, header 
 		blockReward = ETH1BlockReward
 	}
 	if(header.Number.Cmp(config.EthPoWForkBlock) == 0){
-		blockReward = AirDropward
+		blockReward = AirDropReward
 	}
 	// Accumulate the rewards for the miner and any included uncles
 	reward := new(big.Int).Set(blockReward)
